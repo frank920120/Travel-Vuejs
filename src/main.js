1808c35cac2947a122860ad762f1cdd716b4f33a
Frank Fang <frankfang2014@hotmail.com>
2019-11-23 20:43:35 -0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/styles/reset.css";
+import fastClick from "fastclick";
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div>
+    hello world
   </div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  name: "Home"
 };
 </script>
 
-<style></style>
+<style scope></style>
