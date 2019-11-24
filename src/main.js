590f36b37a896fd60400bcfacfbe2528c1089b27
Frank Fang <frankfang2014@hotmail.com>
2019-11-23 23:39:22 -0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from "./router";
 import "./assets/styles/reset.css";
 import "./assets/styles/iconfont.css";
 import fastClick from "fastclick";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
+Vue.use(VueAwesomeSwiper);
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
 
---
+++ src/pages/Home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="list of swipeLists" :key="list.id">
+        <img :src="list.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true
+      },
+      swipeLists: [
+        {
+          id: "0001",
+          imgUrl:
+            "//dimg04.c-ctrip.com/images/70061a0000019887j7064_750_788_391.jpg"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "//dimg04.c-ctrip.com/images/700416000000zfvklE101_750_788_391.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background-color: #fff;
+}
+img {
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+}
+</style>
